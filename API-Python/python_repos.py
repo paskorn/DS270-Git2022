430b71525b333c36b2d65742633e700465a397b5
CMU\paskorn.c <paskorn.c@cmu.ac.th>
2022-09-21 13:54:56 +0700

    add new files

--- API-Python/python_repos.py
+++ API-Python/python_repos.py
@@ -11,15 +11,15 @@ response_dict = r.json()
 print(f"Total repositories: {response_dict['total_count']}")
 
 # Explore information about the repositories.
-repo_dicts = response_dict['items']
-print(f"Repositories returned: {len(repo_dicts)}")
+#repo_dicts = response_dict['items']
+#print(f"Repositories returned: {len(repo_dicts)}")
 
-print("\nSelected information about each repository:")
-for repo_dict in repo_dicts:
-    print(f"Name: {repo_dict['name']}")
-    print(f"Owner: {repo_dict['owner']['login']}")
-    print(f"Stars: {repo_dict['stargazers_count']}")
-    print(f"Repository: {repo_dict['html_url']}")
-    print(f"Created: {repo_dict['created_at']}")
-    print(f"Updated: {repo_dict['updated_at']}")
-    print(f"Description: {repo_dict['description']}")
+#print("\nSelected information about each repository:")
+#for repo_dict in repo_dicts:
+#    print(f"Name: {repo_dict['name']}")
+#    print(f"Owner: {repo_dict['owner']['login']}")
+#    print(f"Stars: {repo_dict['stargazers_count']}")
+#    print(f"Repository: {repo_dict['html_url']}")
+#    print(f"Created: {repo_dict['created_at']}")
+#    print(f"Updated: {repo_dict['updated_at']}")
+#    print(f"Description: {repo_dict['description']}")
